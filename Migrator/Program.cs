b790211778bdf7749fcda132250224a937085770
bspjojo <thompsonjoseph0@gmail.com>
2019-03-07 19:35:01 +1300

    migration (#1)

---
+++ Migrator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using DbUp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Binder;
+
+namespace Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("GameDb");
+            var migrationSection = configuration.GetSection("Migration");
+            var folder = migrationSection.GetValue<string>("FolderPath");
+            var ensureDatabaseExists = migrationSection.GetValue<bool>("EnsureDatabaseExists");
+
+            Console.WriteLine($"Folder: {folder}");
+
+            DatabaseSetup(connectionString, folder, ensureDatabaseExists);
+        }
+
+        private static void DatabaseSetup(string connectionString, string path, bool ensureDatabaseExists)
+        {
+            Console.WriteLine("Beginning migration.");
+
+            if (ensureDatabaseExists)
+            {
+                EnsureDatabase.For.SqlDatabase(connectionString);
+            }
+
+            var upgrader = DeployChanges.To.SqlDatabase(connectionString)
+                .WithTransactionPerScript()
+                .WithScriptsFromFileSystem(path)
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.WriteLine("Migration failed.");
+
+                throw result.Error;
+            }
+            Console.WriteLine("Migration success.");
+        }
+    }
+}
